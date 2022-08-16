d8474586c29e42ed592603efc032720ab6e68061
zumonhossain <zumon15-13169@diu.edu.bd>
2022-08-16 19:30:22 +0600

    Get All Data For Subject

---
+++ app/Http/Controllers/Api/SubjectController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Subject;
+
+
+class SubjectController extends Controller{
+    public function index(){
+        $allSubject = Subject::get();
+        return response()->json(['success' => 'true', 'status_code' => '200', 'data' => $allSubject]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentClassController;
+use App\Http\Controllers\Api\SubjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,22 @@ use App\Http\Controllers\Api\StudentClassController;
 */
 
 
+
+
+// Student Class
 Route::get('/student/class', [StudentClassController::class, 'index']);
 Route::get('/view/student/class/{id}', [StudentClassController::class, 'viewStudentClass']);
 Route::post('/save/student/class', [StudentClassController::class, 'saveStudentClass']);
 Route::post('/update/student/class/{id}', [StudentClassController::class, 'updateStudentClass']);
 Route::get('/delete/student/class/{id}', [StudentClassController::class, 'deleteStudentClass']);
+
+
+// Subject
+Route::get('/subject', [SubjectController::class, 'index']);
+
+
+
+
+
+
+
